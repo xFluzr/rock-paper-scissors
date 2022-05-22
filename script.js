58b20799ae50a7cceeb05bcdd48fcdf8f8ec7d63
Kuba <jakub.jaszcz538@gmail.com>
2022-05-22 20:55:13 +0200

    Scipt version alfa, Added HTML structure

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("Hello World")
+const possibleOutcome=["Paper","Scissors","Rock"];
+const weapons = document.querySelectorAll(".weapon");
+const playBtn = document.querySelector(".play");
+
+weapons.forEach(weapon => {
+    weapon.addEventListener("click",playerPlay)
+});
+
+playBtn.addEventListener("click",game);
+
+
+
+
+
+
+function playerPlay(){
+   console.log(`Player choose`)
+}
+
+function game(){
+    playerPlay()
+    computerPlay()
+}
+
+
+//Computer Play 
+function computerPlay(){
+    console.log(possibleOutcome[random()])
+}
+
+function random(){
+    return Math.floor(Math.random()*3);
+}
+
